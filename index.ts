223f91089b1f550adb62a32982cd1eca8d802826
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-03-16 10:48:31 +0100

    done

--- index.ts
+++ index.ts
@@ -19,6 +19,16 @@ function createToken(id: number) {
     return jwt.sign({ id: id }, process.env.MY_SECRET, { expiresIn: '3days' })
 }
 
+async function getUserFromToken(token: string) {
+    const decodedToken = jwt.verify(token, process.env.MY_SECRET)
+    const user = await prisma.user.findUnique({
+        where: { id: decodedToken.id },
+        include: { transactions: true }
+    })
+    return user
+}
+
+
 app.get('/users', async (req, res) => {
     const users = await prisma.user.findMany({ include: { transactions: true } })
     res.send(users)
@@ -45,12 +55,12 @@ app.post('/sign-up', async (req, res) => {
 
 app.patch('/changePassword', async (req, res) => {
     const { email, password } = req.body
-    const user = await prisma.user.findUnique({ where: { email: email}})
+    const user = await prisma.user.findUnique({ where: { email: email } })
 
     if (user) {
         try {
             const hash = bcrypt.hashSync(password, 8)
-            const updateUser = await prisma.user.update( { where: {email}, data: { password: hash}} )
+            const updateUser = await prisma.user.update({ where: { email }, data: { password: hash } })
             res.send({ updateUser, token: createToken(updateUser.id) })
         } catch (err) {
             // @ts-ignore
@@ -58,6 +68,50 @@ app.patch('/changePassword', async (req, res) => {
         }
     } else res.status(404).send({ error: "User not found" })
 
+})
+
+app.post('/sign-in', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+            include: { transactions: true }
+        })
+        // @ts-ignore
+        const passwordMatches = bcrypt.compareSync(password, user.password)
+
+        if (user && passwordMatches) {
+            const { id, email, fullName, amountInAccount, transactions } = user
+            res.send({ data: { id, email, fullName, amountInAccount, transactions }, token: createToken(user.id) })
+        } else {
+            throw Error()
+        }
+    } catch (err) {
+        res.status(400).send({ error: 'User/password invalid.' })
+    }
+})
+
+app.post('/validate', async (req, res) => {
+    const { token } = req.body
+    try {
+        const user = await getUserFromToken(token)
+        res.send(user)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
+})
+
+app.get('/banking-info', async (req, res) => {
+    const token = req.headers.authorization
+    console.log(token)
+    try {
+        const user = await getUserFromToken(token)
+        const { id, email, fullName, amountInAccount, transactions } = user
+        res.send({ data: { id, email, fullName, amountInAccount, transactions } })
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
 })
 
 app.listen(PORT, () => {
